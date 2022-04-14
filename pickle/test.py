e0ecd12e3d2e4314c181d67a1666ccde3f8cce51
gdharanidharan <61073862+gdharanidharan@users.noreply.github.com>
2022-04-14 01:16:54 -0700

    Add files via upload

---
+++ pickle/test.py
@@ -0,0 +1,11 @@
+import pickle
+
+from matplotlib.pyplot import pcolor
+
+# load the mode
+
+model = pickle.load(open('diabetic_80.pkl','rb'))
+
+result = model.predict([[1,2,3,4,5,6,7,8]])[0]
+
+print(result)
